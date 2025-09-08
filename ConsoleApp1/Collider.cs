264c3b7f1389a4fe78e3000e143435fa4601ce95
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2025-09-09 00:26:02 +0200

    mise en place collider Branchement restant

---
+++ ConsoleApp1/Collider.cs
@@ -0,0 +1,41 @@
+﻿using SceneSys.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SceneSys
+{
+    class Collider : IGameLoop
+    {
+        private int row;    
+        private int col;
+        public Collider(int row, int col)
+        {
+            
+            this.row = row;
+            this.col = col; 
+
+        }
+
+        public bool WallCollider((int x, int y) head) {
+
+            return head.x < 0 || head.y < 0 || head.x > row || head.y > col;
+        }
+
+        public bool ItSelfCollider(Queue<(int x, int y)> head, Queue<(int x, int y)> body) {
+
+            //TODO
+            return false;
+
+        }
+
+
+        public bool FoodCollider((int x, int y) head, (int x, int y) food)
+        {
+            return head.x == food.x && head.y == food.y;
+        }
+
+    }
+}
